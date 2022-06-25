a16daefd3223691a0da77effeeefaea0f14c3d0f
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 19:42:15 +0530

    Initial commit

---
+++ src/vars2.kt
@@ -0,0 +1,24 @@
+fun main(args:Array<String>) {
+
+    /*
+    val i: Int = 10    // Value declaration of type Integer, Initialization and assignment
+    println("$i")
+    val j = 20
+    println(j)
+    */
+
+    /*
+    val i: Long = 99999    // Value declaration of type Long, Initialization and assignment
+    println("$i")
+    val j = 1000000
+    println(j)
+    */
+
+    val i = "A"
+    println("$i")
+    val j: Char    // Value declaration of type Char
+    j = 'B'        // Initialization and assignment
+    println(j)
+
+    println("Third Kotline Prog , By Sanjay D Shirsath")
+}
